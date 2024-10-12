638a2a3ff7cddd556a98cf6475b4b6644c7b8552
shenmo <jifengshenmo@outlook.com>
2024-10-12 11:38:46 +0800

    feat:support debian build

---
+++ translations/zh_CN.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ForcastWidget</name>
+    <message>
+        <location filename="../forcastwidget.cpp" line="43"/>
+        <source>City</source>
+        <translation>城市</translation>
+    </message>
+    <message>
+        <location filename="../forcastwidget.cpp" line="47"/>
+        <source>01-01 Mon</source>
+        <translation>01-01 星期一</translation>
+    </message>
+    <message>
+        <location filename="../forcastwidget.cpp" line="112"/>
+        <source>Clouds</source>
+        <translation>多云</translation>
+    </message>
+    <message>
+        <location filename="../forcastwidget.cpp" line="113"/>
+        <source>Clear</source>
+        <translation>晴</translation>
+    </message>
+    <message>
+        <location filename="../forcastwidget.cpp" line="114"/>
+        <source>Rain</source>
+        <translation>雨</translation>
+    </message>
+    <message>
+        <location filename="../forcastwidget.cpp" line="115"/>
+        <source>Snow</source>
+        <translation>雪</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherPlugin</name>
+    <message>
+        <location filename="../weatherplugin.cpp" line="123"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="129"/>
+        <location filename="../weatherplugin.cpp" line="235"/>
+        <source>Set</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="135"/>
+        <source>Refresh</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="141"/>
+        <location filename="../weatherplugin.cpp" line="197"/>
+        <source>Clouds Map</source>
+        <translation>地图</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="147"/>
+        <source>Log</source>
+        <translation>日志</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="239"/>
+        <source>City</source>
+        <translation>城市</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="248"/>
+        <source>Country</source>
+        <translation>国家</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="260"/>
+        <source>Search your city and country in &lt;a style=&apos;color:white;&apos; href=&apos;https://openweathermap.org&apos;&gt;openweathermap.org&lt;/a&gt;</source>
+        <translation>在 &lt;a style=&apos;color:white;&apos; href=&apos;https://openweathermap.org&apos;&gt;openweathermap.org&lt;/a&gt; 搜索城市</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="290"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="290"/>
+        <source> does not exists !</source>
+        <translation> 不存在！</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="299"/>
+        <source>Temperature Unit</source>
+        <translation>温度单位</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="307"/>
+        <source>Confirm</source>
+        <translation>配置</translation>
+    </message>
+    <message>
+        <location filename="../weatherplugin.cpp" line="308"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+</TS>
